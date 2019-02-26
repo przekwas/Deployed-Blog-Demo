2bf63c1e4dcbe290a6448dfcc20d8983e926a538
przekwas <luke@covalence.io>
2019-02-26 16:27:56 -0600

    adding tags to router

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -4,12 +4,13 @@ import * as passport from 'passport';
 import blogsRouter from './blogs';
 import usersRouter from './users';
 import blogtagsRouter from './blogtags';
+import tagsRouter from './tags';
 
 const router = express.Router();
 
 router.use((req, res, next) => {
     passport.authenticate('bearer', { session: false }, (err, user, info) => {
-        if(user) req.user = user;
+        if (user) req.user = user;
         return next();
     })(req, res, next);
 });
@@ -17,5 +18,6 @@ router.use((req, res, next) => {
 router.use('/blogs', blogsRouter);
 router.use('/users', usersRouter);
 router.use('/blogtags', blogtagsRouter);
+router.use('/tags', tagsRouter);
 
 export default router;
